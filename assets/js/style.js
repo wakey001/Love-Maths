b0760d23f35db7dd26d5738ba81c1c2f6d746dc3
wakey001 <stefanwakeman001@hotmail.co.uk>
2021-10-07 20:41:27 +0000

    set up structures of game functions

---
+++ assets/js/style.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionalQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
